e70a29aa9d43e2ca2ac9759de3505b737a0a8bb9
WeiChien Hsu <weichien711@gmail.com>
2017-10-17 21:02:31 +0000

    route parameter

---
+++ FirstExpressApp/PackageJsonDemo/app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.get("/",function(req, res){
+    res.send("hey");
+})
+
+app.get("*", function(req, res){
+    res.send("There's something wrong");
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server is connecting!");
+})
