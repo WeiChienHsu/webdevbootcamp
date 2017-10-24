c9a090115c857ea1189c8f738a91c012a2025e20
WeiChien Hsu <weichien711@gmail.com>
2017-10-24 01:58:17 +0000

    set search and result pages

--- challenge2/app.js
+++ challenge2/app.js
@@ -1,10 +1,23 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+
+app.use(express.static("public"));
 app.set("view engine","ejs");
 
 //ROUTE Search page (send to search) - > use element input.name
+app.get("/",function(req, res){
+    res.render("search");
+})
 
 //ROUTE Results page (Get the information from api)
+app.get("/results",function(req, res){
+    var data;
+    res.render("results",{data:data});
+    console.log("req.query");
+})
 
 
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("connecting!");
+})
