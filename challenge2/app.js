50f8be027b8b6433b57d48a8e100709c28b9e9a8
WeiChien Hsu <weichien711@gmail.com>
2017-10-24 01:47:29 +0000

    challenge2

---
+++ challenge2/app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine","ejs");
+
+//ROUTE Search page (send to search) - > use element input.name
+
+//ROUTE Results page (Get the information from api)
+
+
