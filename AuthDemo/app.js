6dec37d31afd7450bc9cd2b3dcdf64391a59d65d
WeiChien Hsu <weichien711@gmail.com>
2017-11-02 01:40:36 +0000

    add scret page

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -4,6 +4,10 @@ app.set('view engine','ejs');
 
 app.get("/",function(req,res){
     res.render("home");
+});
+
+app.get("/secret",function(req, res){
+    res.render("secret");
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
