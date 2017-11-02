ae5baf425e063a7c0a2a57f62adf40373c390a20
WeiChien Hsu <weichien711@gmail.com>
2017-11-02 00:51:42 +0000

    add Authentication

---
+++ AuthDemo/app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+app.set('view engine','ejs');
+
+app.get("/",function(req,res){
+    res.render("home");
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("connect!");
+})
