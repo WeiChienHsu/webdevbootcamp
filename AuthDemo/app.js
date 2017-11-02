c0025e2fd19346247454a91e5284133c4aebe8ef
WeiChien Hsu <weichien711@gmail.com>
2017-11-02 12:58:18 +0000

    authen

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,7 +1,7 @@
 var express                 = require("express"),
     mongoose                = require("mongoose"),
     passport                = require("passport"),
-    bodyParse               = require("body-parser"),
+    bodyParser              = require("body-parser"),
     User                    = require("./models/user"),
     LocalStrategy           = require("passport-local"),
     passportLocalMongoose   = require("passport-local-mongoose")
@@ -10,6 +10,7 @@ var app = express();
 
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express-session")({
     secret:"Rusty is the best og in the world",
     resave: false,
@@ -25,12 +26,37 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//========
+// ROUTES
+//=========
+
+
 app.get("/",function(req,res){
     res.render("home");
 });
 
 app.get("/secret",function(req, res){
     res.render("secret");
+});
+
+// Auth Routes
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//handling user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    
+User.register(new User({username:req.body.username}),req.body.password, function(err, user){
+       if(err){
+            console.log(err);
+            return res.render('register');
+            });
+        passport.authenticate("local")(req, res, function(){
+
+       } 
+    });
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
