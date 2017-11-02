583005d99b072be3110bc02e19d716b2277486a2
WeiChien Hsu <weichien711@gmail.com>
2017-11-02 09:25:41 +0000

    set up passport

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,6 +1,29 @@
-var express = require("express");
+var express                 = require("express"),
+    mongoose                = require("mongoose"),
+    passport                = require("passport"),
+    bodyParse               = require("body-parser"),
+    User                    = require("./models/user"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose")
+    
 var app = express();
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
+app.use(require("express-session")({
+    secret:"Rusty is the best og in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.set('view engine','ejs');
+//
+app.use(passport.initialize());
+app.use(passport.session());
+// 
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
     res.render("home");
